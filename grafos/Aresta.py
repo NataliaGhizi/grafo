289b52262ba515aba2011f86e9bd0c1ff0755ce7
rafael zanatta <rafaelzanatta91@gmail.com>
2019-06-03 23:01:19 -0300

    removido peso

--- grafos/Aresta.py
+++ grafos/Aresta.py
@@ -1,19 +1,26 @@
 class Aresta():
-	def __init__(self,origem,destino):
+	def __init__(self,origem,destino,peso=0):
 		self.origem = origem
 		self.destino = destino
+		self.peso = peso
 				
 	def getOrigem(self):
 		return self.origem
 		
 	def getDestino(self):
 		return self.destino
+
+	def getPeso(self):
+		return self.peso
 			
 	def setOrigem(self,vertice):
 		self.origem = vertice
 		
 	def setDestino(self,vertice):
 		self.destino = vertice
+
+	def setPeso(self, peso):
+		self.peso = peso
 	
 	def __str__(self):
-		return '{}-->{}'.format(self.origem.getId(), self.destino.getId())
+		return '{}-->{}-p:{}'.format(self.origem.getId(), self.destino.getId(), self.peso.getId())
